569811b51c6ea81dc1a681fad652f3ba2cb98910
abhiraftaar007 <singhabhishekas786@gmail.com>
2025-01-15 01:31:24 +0530

    Longest Subarray with Sum K with Brute force and PreSum Approach

---
+++ Sliding window/LongestSubarrayWithSumK.cpp
@@ -0,0 +1,70 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// Brute Force
+int longestSubarray(vector<int> &arr, int k)
+{
+    // code here
+    // Brute Force - TLE
+    int n = arr.size(), ans = 0, sum = 0;
+    for (int i = 0; i < n - 1; i++)
+    {
+        sum = 0;
+        for (int j = i; j < n; j++)
+        {
+            sum += arr[j];
+            if (sum == k)
+                ans = max(ans, j - i + 1);
+        }
+    }
+
+    return ans;
+}
+
+// Optimal
+int longestSubarray2(vector<int> &a, int k)
+{
+    // code here
+    int n = a.size(); // size of the array.
+
+    map<int, int> preSumMap;
+    int sum = 0;
+    int maxLen = 0;
+    for (int i = 0; i < n; i++)
+    {
+        // calculate the prefix sum till index i:
+        sum += a[i];
+
+        // if the sum = k, update the maxLen:
+        if (sum == k)
+        {
+            maxLen = max(maxLen, i + 1);
+        }
+
+        // calculate the sum of remaining part i.e. x-k:
+        int rem = sum - k;
+
+        // Calculate the length and update maxLen:
+        if (preSumMap.find(rem) != preSumMap.end())
+        {
+            int len = i - preSumMap[rem];
+            maxLen = max(maxLen, len);
+        }
+
+        // Finally, update the map checking the conditions:
+        if (preSumMap.find(sum) == preSumMap.end())
+        {
+            preSumMap[sum] = i;
+        }
+    }
+
+    return maxLen;
+}
+
+int main()
+{
+    vector<int> arr = {10,5,2,7,1,-10};
+    int k = 15;
+    cout << longestSubarray2(arr,k);
+    return 0;
+}
