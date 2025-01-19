76ce2056a7244e98f1ed0926bd232d232f836aaf
Abhishek <68496907+abhiraftaar007@users.noreply.github.com>
2025-01-19 12:31:34 +0530

    Update IncreasingTripletSubsequence.cpp

--- Arrays/IncreasingTripletSubsequence.cpp
+++ Arrays/IncreasingTripletSubsequence.cpp
@@ -1,6 +1,8 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+// https://leetcode.com/problems/increasing-triplet-subsequence/description/
+
 // Brute Force - TLE
 bool increasingTriplet1(vector<int> &nums)
 {
