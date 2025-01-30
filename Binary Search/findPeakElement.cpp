411558bf815c324fd9029da31a8c8efee01bee70
abhiraftaar007 <singhabhishekas786@gmail.com>
2025-01-30 16:21:33 +0530

    Find peak element using binary search

--- Binary Search/findPeakElement.cpp
+++ Binary Search/findPeakElement.cpp
@@ -1,4 +1,4 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 // https://leetcode.com/problems/find-peak-element/description/?envType=study-plan-v2&envId=leetcode-75
@@ -38,10 +38,39 @@ int findPeakElement(vector<int> &nums)
     return 0;
 }
 
+// Optimal - Binary Search
+int findPeakElement2(vector<int> &arr)
+{
+    int n = arr.size();
+    if (n == 1)
+        return 0;
+
+    if (arr[0] > arr[1])
+        return 0;
+    if (arr[n - 1] > arr[n - 2])
+        return n - 1;
+
+    int low = 1, high = n - 2;
+    while (low <= high)
+    {
+        int mid = (low + high) / 2;
+        if (arr[mid] > arr[mid - 1] && arr[mid] > arr[mid + 1])
+        {
+            return mid;
+        }
+        else if (arr[mid] > arr[mid - 1])
+            low = mid + 1;
+        else
+            high = mid - 1;
+    }
+
+    return -1;
+}
+
 int main()
 {
-    vector<int> nums = {1,2,1,3,5,6,4};
-    cout << findPeakElement(nums);
+    vector<int> nums = {1, 2, 1, 3, 5, 6, 4};
+    cout << findPeakElement2(nums);
 
     return 0;
 }
