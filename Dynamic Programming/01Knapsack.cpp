4fcdef3b9d28e9459c2ef4365e829ef559ae36cd
abhiraftaar007 <singhabhishekas786@gmail.com>
2025-02-03 15:17:58 +0530

    0/1 Knapsack Top down Approach added

--- Dynamic Programming/01Knapsack.cpp
+++ Dynamic Programming/01Knapsack.cpp
@@ -26,17 +26,48 @@ int knapSack(int wt[], int val[], int w, int n)
     }
 }
 
+// Top Down Approach -
+int knapSack2(int wt[], int val[], int w, int n)
+{
+    int t[n + 1][w + 1];
+    for (int i = 0; i < n + 1; i++)
+    {
+        for (int j = 0; j < w + 1; j++)
+        {
+            if (i == 0 || j == 0)
+                t[i][j] = 0;
+        }
+    }
+
+    for (int i = 1; i < n + 1; i++)
+    {
+        for (int j = 1; j < w + 1; j++)
+        {
+            if (wt[i - 1] <= j)
+            {
+                t[i][j] = max(val[i - 1] + t[i - 1][j - wt[i - 1]], t[i - 1][j]);
+            }
+            else
+                t[i][j] = t[i - 1][j];
+        }
+    }
+
+    return t[n][w];
+}
+
 int main()
 {
-    int wt[4] = {5, 4, 6, 3};
-    int val[4] = {10, 40, 30, 50};
+    int wt[4] = {1, 3, 4, 5};
+    int val[4] = {1, 4, 5, 7};
     int n = sizeof(wt) / sizeof(wt[0]);
     // int n = wt.size(); for vector
-    int w = 5;
+    int w = 7;
 
     memset(t, -1, sizeof(t));
 
-    cout << knapSack(wt, val, w, n);
+    cout << knapSack(wt, val, w, n) << '\n';
+
+    cout << knapSack2(wt, val, w, n);
 
     return 0;
 }
